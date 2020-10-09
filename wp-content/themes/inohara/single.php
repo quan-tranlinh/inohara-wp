9b22c577b4a1d363dcf6bd3a961d9c5ee7edbe6f
tran linh quan <quantranlinh.dev@gmail.com>
2020-10-09 10:20:08 +0700

    Update UI Single page.

--- wp-content/themes/inohara/single.php
+++ wp-content/themes/inohara/single.php
@@ -2,15 +2,29 @@
 get_header();
 ?>
     <div class="main">
+		<div class="page_title01">
+			<h2 class="title01"><?php echo get_the_title() ?></h2>
+			<p class="sub_title01"><?php echo get_the_title() ?></p>
+		</div><!--page_title01-->
+
+		<div class="breadcrumb">
+			<div class="inner">
+				<ul>
+					<li class="breadcrumb_item"><a href="<?php echo home_url(); ?>/">トップ</a></li>
+					<li class="breadcrumb_item"><?php echo get_the_title() ?></li>
+				</ul>
+			</div>
+		</div><!--breadcrumb-->
+
 		<div class="inner">
 			<?php
 			while ( have_posts() ) :
 				the_post();
 				get_template_part( 'template-parts/content', get_post_type() );
-			endwhile; // End of the loop.
+			endwhile;
 			?>
 			
 		</div>
-    </div><!-- #main -->
+    </div>
 <?php
 get_footer();
